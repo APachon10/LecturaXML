9b8749168e191e610592efeb84d252c7f5533890
APachon10 <Pachondestroy95@gmail.com>
2020-11-10 18:05:20 +0100

    Leer Fichero completado

--- src/Test/Demo.java
+++ src/Test/Demo.java
@@ -19,17 +19,20 @@ public class Demo {
 			Document document = documentBuilder.parse(archivo);
 			document.getDocumentElement().normalize();
 			System.out.println("Elemento raiz:" + document.getDocumentElement().getNodeName());
+			System.out.println("==============================");
 			NodeList listaEmpleados = document.getElementsByTagName("Anime");
 			for (int temp = 0; temp < listaEmpleados.getLength(); temp++) {
 				Node nodo = listaEmpleados.item(temp);
 				System.out.println("Elemento:" + nodo.getNodeName());
 				if (nodo.getNodeType() == Node.ELEMENT_NODE) {
 					Element element = (Element) nodo;
-					System.out.println("id: " + element.getAttribute("id"));
+					System.out.println("id: " + element.getElementsByTagName("id").item(0).getTextContent());
 					System.out.println("Nombre: " + element.getElementsByTagName("name").item(0).getTextContent());
 					System.out.println("Numero Capitulos : "+element.getElementsByTagName("num_chapters").item(0).getTextContent());
+					System.out.println("==============================");
 				}
 			}
+			System.out.println("==============================");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
